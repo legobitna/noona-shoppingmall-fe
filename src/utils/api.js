d3d21d8ef074ad8a3086ed821b1eb5db56b6c1a2
Bitna Kim <tallshe108@hanmail.net>
2023-03-27 22:01:10 +0700

    modify space

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+console.log("proxy", BACKEND_PROXY);
 const api = axios.create({
   baseURL: BACKEND_PROXY,
   headers: {
